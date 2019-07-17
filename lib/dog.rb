16bbca72b3a7e6a34fa9b5923b13b520c5b713f0
Martin Crabtree <martin.crabtree@gmail.com>
2019-07-17 01:02:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,4 +11,16 @@ class Dog
     @name = new_name
   end
   
+  def initialize(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  
 end
